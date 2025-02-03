fdfeac4a9efd9d00b3560def177fa483500f6db1
Bruno Kemmer <brunokemmer@gmail.com>
2025-02-03 11:18:49 -0300

    changed gnu stow installation

--- scripts/install_stow.sh
+++ scripts/install_stow.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-
 # steps from grencez.dev/2016/stow-tutorial-20160505
-[[ -d ~/.local/src ]] || mkdir ~/.local/src
-cd ~/.local/src
-curl -O http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
-tar xf stow-latest.tar.gz
+
+DOWNLOAD_FOLDER="$HOME/Downloads"
+# cd ~/.local/src
+URL="http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz"
+FNAME="stow-latest.tar.gz"
+[[ -e $DOWNLOAD_FOLDER/$FNAME ]] && rm $DOWNLOAD_FOLDER/$FNAME
+read -s -k "?Download $URL and press a key to continue..."
+echo ""
+cd $DOWNLOAD_FOLDER
+# curl -O http://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
+tar xf $FNAME
 release=$(ls -Avr | grep -m1 -axEe 'stow-[0-9.]+')
 cd $release
 
@@ -13,5 +19,6 @@ make
 make install
 
 cd ~/.local/stow
-cd ~/.local/src && rm -fr $release stow-latest.tar.gz
+./$release/bin/stow $release
+cd $DOWNLOAD_FOLDER && rm -fr $release $DOWNLOAD_FOLDER
 
