ed437c33a78a1396042923fcf15d21e408808310
Bruno Kemmer <brunokemmer@gmail.com>
2025-02-04 11:32:35 -0300

    Fixed the wrong arg in zsh read and sourcing .function under vim script

--- scripts/vim/vim_extensions_without_git_clone.sh
+++ scripts/vim/vim_extensions_without_git_clone.sh
@@ -1,39 +1,21 @@
 #!/usr/bin/env zsh
+source $HOME/.functions
+type dl_and_mv_repo &>/dev/null && echo "dl_and_mv_repo() found" || { echo "dl_and_mv_repo() not found, check ~/.functions"; exit 1; }
 
-dl_and_mv_repo(){
-    URL=$1              # url to zip file with the master branch of the repository
-    ZIP=$2              # name of the zip and unziped folder ex.: dot-file-main.zip and dot-file-main
-    DEST=$3             # name of the destination folder
-    DOWNLOAD_FOLDER=$4  # path to the download folder
-    INSTALL_FOLDER=$5   # path to be installed  
-
-    echo ""
-    [[ -e $DOWNLOAD_FOLDER/$ZIP.zip ]] && rm $DOWNLOAD_FOLDER/$ZIP.zip 
-    read -s -k "?Download $URL and press a key to continue..."
-    echo ""
-    cd $DOWNLOAD_FOLDER
-    [[ -e $DOWNLOAD_FOLDER/$ZIP.zip ]] || { echo "$ZIP.zip file not found. Exiting"; exit 1; }
-    unzip -qq $ZIP.zip
-    mv $ZIP $DEST
-    rm -r $INSTALL_FOLDER/$DEST
-    mv $DEST $INSTALL_FOLDER/$DEST
-    rm $ZIP.zip
-    echo ""
-}
-# TODO: make this reusable
-# type dl_and_mv_repo &>/dev/null && echo "dl_and_mv_repo() found" || { echo "dl_and_mv_repo() not found, check ~/.functions"; exit 1; }
-
+echo ""
 echo "VIMCONFIG: $VIMCONFIG"
 
 VIM_EXTENSIONS_PATH="$VIMCONFIG/pack/plugins"
 echo "Installing plugins on $VIM_EXTENSIONS_PATH"
+
+[[ -d $VIM_EXTENSIONS_PATH ]] || mkdir -p $VIM_EXTENSIONS_PATH/{start,opt}
 
 DOWNLOAD_FOLDER=$HOME/Downloads
 
 # Installing jedi-vim
 dl_and_mv_repo https://github.com/davidhalter/jedi-vim/archive/refs/heads/master.zip jedi-vim-master jedi-vim $DOWNLOAD_FOLDER $VIM_EXTENSIONS_PATH/start
 # installing its submodules
-JEDI_PYTHONX_PATH=$VIM_EXTENSIONS_PATH/start/jedi-vim/pythonx/
+JEDI_PYTHONX_PATH=$VIM_EXTENSIONS_PATH/start/jedi-vim/pythonx
 # jedi
 rm -r $JEDI_PYTHONX_PATH/jedi
 dl_and_mv_repo https://github.com/davidhalter/jedi/archive/refs/heads/master.zip jedi-master jedi $DOWNLOAD_FOLDER $JEDI_PYTHONX_PATH
