7842a8b8a108eb692f2d799dcd84e4df6faee1d7
Bruno Kemmer <brunokemmer@gmail.com>
2025-02-18 15:01:33 -0300

    Added a second try on unzip

--- scripts/vim/install_vim_plugins.py
+++ scripts/vim/install_vim_plugins.py
@@ -48,7 +48,8 @@ if not CONFIG_PATH.is_file():
 
 config = configparser.ConfigParser()
 config.read(CONFIG_PATH)
-print(f"List of plugins in {CONFIG_FNAME}: {config.sections()}")
+plugins_list = [s for s in config.sections() if s not in configs_dict['SKIP_SECTIONS']]
+print(f"List of plugins in {CONFIG_FNAME}: {plugins_list}")
 
 VIMCONFIG = os.getenv("VIMCONFIG")
 HOME = os.getenv("HOME")
@@ -64,48 +65,52 @@ OPT_PATH.mkdir(exist_ok=True, parents=True)
 
 interactive_mode, manual_mode = setup_args()
 existing_plugins_list = []
-for plugin_name in config.sections():
-    if plugin_name not in configs_dict["SKIP_SECTIONS"]:
-        plugin_config = config[plugin_name]
+for plugin_name in plugins_list:
+    plugin_config = config[plugin_name]
 
-        mode = plugin_config['mode']
-        assert mode in configs_dict["EXPECTED_MODE_LIST"]
+    mode = plugin_config['mode']
+    assert mode in configs_dict["EXPECTED_MODE_LIST"]
 
-        url = plugin_config['url']
-        url_zip = plugin_config['url_zip']
+    url = plugin_config['url']
+    url_zip = plugin_config['url_zip']
 
-        zip_file = Path(plugin_config['zip_file'])
-        assert zip_file.suffix == ".zip"
+    zip_file = Path(plugin_config['zip_file'])
+    assert zip_file.suffix == ".zip"
 
-        destination_folder_name = plugin_config['destination_folder_name']
+    destination_folder_name = plugin_config['destination_folder_name']
 
-        # optionals
-        extra_cmd = plugin_config.get('extra_cmd')
-        just_in_manual = plugin_config.get('just_in_manual')
+    # optionals
+    extra_cmd = plugin_config.get('extra_cmd')
+    just_in_manual = plugin_config.get('just_in_manual')
 
 
-        DESTINATION_PATH = EXT_PATH / mode / destination_folder_name
-        if interactive_mode:
-            ask_dir_exists(DESTINATION_PATH)
-        elif DESTINATION_PATH.is_dir():
-            existing_plugins_list.append(f"{plugin_name}: {str(DESTINATION_PATH)}")
-            continue
+    DESTINATION_PATH = EXT_PATH / mode / destination_folder_name
+    if interactive_mode:
+        ask_dir_exists(DESTINATION_PATH)
+    elif DESTINATION_PATH.is_dir():
+        existing_plugins_list.append(f"{plugin_name}: {str(DESTINATION_PATH)}")
+        continue
 
-        print(f"\nInstalling {plugin_name}")
-        if manual_mode:
-            # No git clone in this mode
-            print(f'Download {url_zip} waiting for it under {DOWNLOAD_PATH} with filename: {zip_file}')
-            file_path = DOWNLOAD_PATH/zip_file
-            while True:
-                if file_path.is_file():
-                    print(f"Found {file_path}")
+    print(f"\nInstalling {plugin_name}")
+    if manual_mode:
+        # No git clone in this mode
+        print(f'Download {url_zip} waiting for it under {DOWNLOAD_PATH} with filename: {zip_file}')
+        file_path = DOWNLOAD_PATH/zip_file
+        while True:
+            if file_path.is_file():
+                print(f"Found {file_path}")
+                try:
                     unzip(file_path, DOWNLOAD_PATH)
-                    shutil.move(DOWNLOAD_PATH/ zip_file.with_suffix(''), DESTINATION_PATH)
-                    break
-                time.sleep(SECONDS_TO_CHECK_FOR_DOWNLOAD_FILES)
-        else:
-            cmd = f"git clone --recursive --depth 1 {url} {DESTINATION_PATH}"
-            r = os.system(cmd)
+                except zipfile.BadZipFile:
+                    # Bad zip wait 2 seconds and try a second time. Maybe it was still saving the file.
+                    time.sleep(2)
+                    unzip(file_path, DOWNLOAD_PATH)
+                shutil.move(DOWNLOAD_PATH/ zip_file.with_suffix(''), DESTINATION_PATH)
+                break
+            time.sleep(SECONDS_TO_CHECK_FOR_DOWNLOAD_FILES)
+    else:
+        cmd = f"git clone --recursive --depth 1 {url} {DESTINATION_PATH}"
+        r = os.system(cmd)
 
 # update helptag"s
 os.system("vim -u NONE -c 'helptags ALL' -c q")
