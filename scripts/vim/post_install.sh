196cc37b1f95e8340506f29259bd19ee352eadc8
Bruno Kemmer <brunokemmer@gmail.com>
2025-02-20 21:57:38 -0300

    added post installation scripts

--- scripts/vim/post_install.sh
+++ scripts/vim/post_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Pasting lightline onedark colorscheme
 LIGHTLINE_COLORSCHEME_PATH="$VIMCONFIG/pack/plugins/start/lightline/autoload/lightline/colorscheme"
-
 [[ -d $LIGHTLINE_COLORSCHEME_PATH ]] && cp "extras/onedark.vim" $LIGHTLINE_COLORSCHEME_PATH/
+
+# Moding coc package.json to CoC
+COC_EXTENSIONS_PATH="$HOME/.config/coc/extensions"
+[[ -d $COC_EXTENSIONS_PATH ]] && cp "extras/package.json" $COC_EXTENSIONS_PATH/
