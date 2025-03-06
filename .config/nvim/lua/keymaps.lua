9f772be176e7356b7589f34beea15ce32372daa2
Bruno Kemmer <brunokemmer@gmail.com>
2025-03-06 07:59:13 -0300

    migration from coc to lsp

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,47 +1,50 @@
+
+local km = vim.keymap
+
 -- leader key to space
-vim.keymap.set("n", "<space>", "<Nop>", {silent = true})
+km.set("n", "<space>", "<Nop>", {silent = true})
 vim.g.mapleader = " "
 
 -- save files
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
+km.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save'})
 
 -- Select all text
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+km.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 
 -- " 'Q' in normal mode enters Ex mode. You almost never want this.-
-vim.keymap.set("n", "Q", "<Nop>", {silent = true})
+km.set("n", "Q", "<Nop>", {silent = true})
 
 -- Alias to replace all to S
-vim.keymap.set("n", "S", ":%s//g<Left><Left>", {desc = "Search"})
+km.set("n", "S", ":%s//g<Left><Left>", {desc = "Search"})
 
 -- Open current window in a new tab | Use <C-W><C-Q> to go back
-vim.keymap.set("n", "<Leader>wo", ":tab split<CR>")
+km.set("n", "<Leader>wo", ":tab split<CR>")
 
 -- Shortcut for :tabnew
-vim.keymap.set("n", "<C-t>", ":tabnew<Space>")
+km.set("n", "<C-t>", ":tabnew<Space>")
 
 -- Preventing bad habits (in normal mode):
-vim.keymap.set("n", "<Left>", ":echoe 'Use h'<CR>")
-vim.keymap.set("n", "<Right", ":echoe 'Use l'<CR>")
-vim.keymap.set("n", "<Up>", ":echoe 'Use k'<CR>")
-vim.keymap.set("n", "<Down>", ":echoe 'Use j'<CR>")
+km.set("n", "<Left>", ":echoe 'Use h'<CR>")
+km.set("n", "<Right", ":echoe 'Use l'<CR>")
+km.set("n", "<Up>", ":echoe 'Use k'<CR>")
+km.set("n", "<Down>", ":echoe 'Use j'<CR>")
 -- Preventing bad habits (in insert mode):
-vim.keymap.set("n", "<Left>", ":echoe 'Use h'<CR>")
-vim.keymap.set("n", "<Right", ":echoe 'Use l'<CR>")
-vim.keymap.set("n", "<Up>", ":echoe 'Use k'<CR>")
-vim.keymap.set("n", "<Down>", ":echoe 'Use j'<CR>")
+km.set("n", "<Left>", ":echoe 'Use h'<CR>")
+km.set("n", "<Right", ":echoe 'Use l'<CR>")
+km.set("n", "<Up>", ":echoe 'Use k'<CR>")
+km.set("n", "<Down>", ":echoe 'Use j'<CR>")
 
 -- Use <C-[hjkl] to select the active split:
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+km.set("n", "<c-k>", ":wincmd k<CR>")
+km.set("n", "<c-j>", ":wincmd j<CR>")
+km.set("n", "<c-h>", ":wincmd h<CR>")
+km.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Creates a 'global' paste from clipboard
-vim.keymap.set("n", "gp", '"+p')
+km.set("n", "gp", '"+p')
 
 -- Move up and down centering the cursos in the middle of the screen
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
+km.set("n", "<C-u>", "<C-u>zz")
+km.set("n", "<C-d>", "<C-d>zz")
 
-
+  
--- scripts/vim/post_install.sh
+++ scripts/vim/post_install.sh
@@ -4,6 +4,6 @@
 LIGHTLINE_COLORSCHEME_PATH="$VIMCONFIG/pack/plugins/start/lightline/autoload/lightline/colorscheme"
 [[ -d $LIGHTLINE_COLORSCHEME_PATH ]] && cp "extras/onedark.vim" $LIGHTLINE_COLORSCHEME_PATH/
 
-# Moding coc package.json to CoC
-COC_EXTENSIONS_PATH="$HOME/.config/coc/extensions"
-[[ -d $COC_EXTENSIONS_PATH ]] && cp "extras/package.json" $COC_EXTENSIONS_PATH/
+# Install pyright
+npm i -g pyright
+
