d13f7a34693d19c492e868fec018cf195524e50e
Bruno Kemmer <brunokemmer@gmail.com>
2025-03-10 17:07:02 -0300

    now f12

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,18 @@
- hs.hotkey.bind({"cmd"}, "f12", function()
-    local app = hs.application.get("kitty")
-    if app then
-        if not app:mainWindow() then
-            app:selectMenuItem({"kitty", "New OS window"})
-        elseif app:isFrontmost() then
-            app:hide()
-        else
-            app:activate()
-        end
-    else
-        hs.application.launchOrFocus("kitty")
-    end
-end)
+hs.hotkey.bind({}, "F12", function()
+  local app = hs.application.get("kitty")
 
+  if app then
+      if not app:mainWindow() then
+          app:selectMenuItem({"kitty", "New OS window"})
+      elseif app:isFrontmost() then
+          app:hide()
+      else
+          app:activate()
+      end
+  else
+      hs.application.launchOrFocus("kitty")
+      app = hs.application.get("kitty")
+  end
+
+  app:mainWindow().setShadows(false)
+end)
