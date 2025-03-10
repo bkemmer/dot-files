91257d99c64eef79fcccdbe7a4e35e78f02fb0b4
Bruno Kemmer <brunokemmer@gmail.com>
2025-03-10 17:01:56 -0300

    added hamerspoon command to open kitty using cmd + f12

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,15 @@
+ hs.hotkey.bind({"cmd"}, "f12", function()
+    local app = hs.application.get("kitty")
+    if app then
+        if not app:mainWindow() then
+            app:selectMenuItem({"kitty", "New OS window"})
+        elseif app:isFrontmost() then
+            app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus("kitty")
+    end
+end)
+
