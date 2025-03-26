2bf2a9012781ccb9443d56b80b516b6d2cd39f94
Bruno Kemmer <brunokemmer@gmail.com>
2025-03-26 18:32:51 -0300

    Added lua LSP and molten bidings

--- .config/nvim/lua/plugins/molten-config.lua
+++ .config/nvim/lua/plugins/molten-config.lua
@@ -8,3 +8,21 @@ vim.keymap.set("n", "<localleader>ip", function()
     vim.cmd(":MoltenInit python3")
   end
 end, { desc = "Initialize Molten for python3", silent = true })
+
+vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>",
+    { silent = true, desc = "Initialize the plugin" })
+vim.keymap.set("n", "<localleader>e", ":MoltenEvaluateOperator<CR>",
+    { silent = true, desc = "run operator selection" })
+vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>",
+    { silent = true, desc = "evaluate line" })
+vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>",
+    { silent = true, desc = "re-evaluate cell" })
+vim.keymap.set("v", "<localleader>r", ":<C-u>MoltenEvaluateVisual<CR>gv",
+    { silent = true, desc = "evaluate visual selection" })
+
+vim.keymap.set("n", "<localleader>rd", ":MoltenDelete<CR>",
+    { silent = true, desc = "molten delete cell" })
+vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>",
+    { silent = true, desc = "hide output" })
+vim.keymap.set("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>",
+    { silent = true, desc = "show/enter output" })
