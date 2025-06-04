ac5fab642e682e98fe837251e8aaea9873a979f2
Bruno Kemmer <brunokemmer@gmail.com>
2025-06-04 12:26:24 -0300

    Working version of blink super-tab

--- .config/nvim/lua/config/mini_deps.lua
+++ .config/nvim/lua/config/mini_deps.lua
@@ -13,7 +13,7 @@
 --
 --
 -- -- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath('data') .. '/site-bak/'
+local path_package = vim.fn.stdpath('data') .. '/site/'
 local mini_path = path_package .. 'pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
 --   print("manual_installs: ");print(manual_installs)
@@ -34,42 +34,42 @@ if not vim.loop.fs_stat(mini_path) then
 end
 
 
---
--- -- Set up 'mini.deps'
--- local MiniDeps = require('mini.deps')
--- MiniDeps.setup({ path = { package = path_package } })
---
--- -- This is a hack to echo the repository zip link if it can't clone it
--- -- nvim --cmd "lua manual_installs=true"
--- -- Therefore, I am replacing the original plugs_install function
--- MiniDeps.plugs_install = function(plugs)
---   if manual_installs then
---     for k,v in pairs(plugs) do
---     local input = vim.fn.input('Enter something: ')
---       vim.notify(k .. ": " .. v)
---     end
---   else
---     return MiniDeps.plugs_install(plugs)
+
+-- Set up 'mini.deps'
+local MiniDeps = require('mini.deps')
+MiniDeps.setup({ path = { package = path_package } })
+
+-- This is a hack to echo the repository zip link if it can't clone it
+-- nvim --cmd "lua manual_installs=true"
+-- Therefore, I am replacing the original plugs_install function
+MiniDeps.plugs_install = function(plugs)
+  if manual_installs then
+    for k,v in pairs(plugs) do
+    local input = vim.fn.input('Enter something: ')
+      vim.notify(k .. ": " .. v)
+    end
+  else
+    return MiniDeps.plugs_install(plugs)
+  end
+end
+
+-- function(plugs)
+--   -- Clone
+--   local prepare = function(p)
+--     if p.source == nil and #p.job.err == 0 then p.job.err = { 'SPECIFICATION HAS NO `source` TO INSTALL PLUGIN.' } end
+--     p.job.command = H.git_cmd('clone', p.source or '', p.path)
+--     p.job.exit_msg = string.format('Installed `%s`', p.name)
 --   end
+--   H.plugs_run_jobs(plugs, prepare)
+--
+--   -- Checkout
+--   vim.tbl_map(function(p) p.job.cwd = p.path end, plugs)
+--   H.plugs_checkout(plugs, { exec_hooks = false, all_helptags = true })
+--
+--   -- Show warnings and errors
+--   H.plugs_show_job_notifications(plugs, 'installing plugin')
 -- end
 --
--- -- function(plugs)
--- --   -- Clone
--- --   local prepare = function(p)
--- --     if p.source == nil and #p.job.err == 0 then p.job.err = { 'SPECIFICATION HAS NO `source` TO INSTALL PLUGIN.' } end
--- --     p.job.command = H.git_cmd('clone', p.source or '', p.path)
--- --     p.job.exit_msg = string.format('Installed `%s`', p.name)
--- --   end
--- --   H.plugs_run_jobs(plugs, prepare)
--- --
--- --   -- Checkout
--- --   vim.tbl_map(function(p) p.job.cwd = p.path end, plugs)
--- --   H.plugs_checkout(plugs, { exec_hooks = false, all_helptags = true })
--- --
--- --   -- Show warnings and errors
--- --   H.plugs_show_job_notifications(plugs, 'installing plugin')
--- -- end
--- --
---
--- return MiniDeps
+
+return MiniDeps
 
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -11,6 +11,39 @@ require("blink.cmp").setup({
   --     download = true, -- false,
   --   },
   -- },
+  keymap = {
+    preset = 'super-tab',
+      ["<S-k>"] = { "scroll_documentation_up", "fallback" },
+      ["<S-j>"] = { "scroll_documentation_down", "fallback" }
+  },
+  completion = {
+  menu = {
+    draw = {
+      components = {
+        kind_icon = {
+          text = function(ctx)
+            local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind) return kind_icon
+          end,
+          -- (optional) use highlights from mini.icons
+          highlight = function(ctx)
+            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+            return hl
+          end,
+        },
+        kind = {
+          -- (optional) use highlights from mini.icons
+          highlight = function(ctx)
+            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+            return hl
+          end,
+        }
+      }
+    }
+  },
+  trigger = {
+    show_in_snippet = false,
+  },
+},
   fuzzy = { implementation = "prefer_rust_with_warning" },
   snippets = { preset = 'mini_snippets' },
     -- ensure you have the `snippets` source (enabled by default)
@@ -22,52 +55,7 @@ require("blink.cmp").setup({
             module = "blink-cmp-copilot",
             score_offset = 100,
             async = true,
-            -- transform_items = function(_, items)
-            --   local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-            --   local kind_idx = #CompletionItemKind + 1
-            --   CompletionItemKind[kind_idx] = "Copilot"
-            --   for _, item in ipairs(items) do
-            --     item.kind = kind_idx
-            --   end
-            --   return items
-            -- end,
           },
-        },
-    },
-    appearance = {
-        -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
-        kind_icons = {
-          Copilot = "",
-          Text = '󰉿',
-          Method = '󰊕',
-          Function = '󰊕',
-          Constructor = '󰒓',
-
-          Field = '󰜢',
-          Variable = '󰆦',
-          Property = '󰖷',
-
-          Class = '󱡠',
-          Interface = '󱡠',
-          Struct = '󱡠',
-          Module = '󰅩',
-
-          Unit = '󰪚',
-          Value = '󰦨',
-          Enum = '󰦨',
-          EnumMember = '󰦨',
-
-          Keyword = '󰻾',
-          Constant = '󰏿',
-
-          Snippet = '󱄽',
-          Color = '󰏘',
-          File = '󰈔',
-          Reference = '󰬲',
-          Folder = '󰉋',
-          Event = '󱐋',
-          Operator = '󰪚',
-          TypeParameter = '󰬛',
         },
     },
 })
