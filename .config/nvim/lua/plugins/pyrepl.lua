e8f1c10c34961886e763b1134bbe5c13f735fcc6
Bruno Kemmer <brunokemmer@gmail.com>
2026-04-12 21:39:55 -0300

    Changes to pyrepl

--- .config/nvim/lua/plugins/pyrepl.lua
+++ .config/nvim/lua/plugins/pyrepl.lua
@@ -7,7 +7,8 @@ vim.pack.add({
 })
 
 local pyrepl = require("pyrepl")
-
+local venv_python_path = '~/.venv_nvim/bin'
+local python_path = vim.fn.isdirectory(venv_python_path) == 1 and vim.fs.joinpath(venv_python_path, "python") or "python"
 -- default config
 pyrepl.setup({
   split_horizontal = false,
@@ -23,7 +24,7 @@ pyrepl.setup({
   image_provider = "placeholders",
   -- can also be a function for advanced use cases
   cell_pattern = "^#.?%%.*$",
-  python_path = "python",
+  python_path = python_path,
   preferred_kernel = "python3",
   -- automatically prompt to convert notebook files into python scripts
   jupytext_hook = true,
@@ -47,3 +48,8 @@ vim.keymap.set("n", "<leader>jp", pyrepl.step_cell_backward, {desc = 'Step Cell 
 vim.keymap.set("n", "<leader>jn", pyrepl.step_cell_forward, {desc = 'Step Cell Forward'})
 vim.keymap.set("n", "<leader>je", pyrepl.export_to_notebook, {desc = 'Step Cell Backward'})
 vim.keymap.set("n", "<leader>js", ":PyreplInstall", {desc = 'PyrepInstall'})
+
+vim.keymap.set("n", "<leader>jl", function()
+  vim.cmd("PyreplSendCell")
+  vim.cmd("PyreplStepCellForward")
+end)
