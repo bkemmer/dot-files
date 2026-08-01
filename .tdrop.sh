1fcc88c54eb71822c83bb12601f51d211efc3c81
Bruno Kemmer <brunokemmer@gmail.com>
2026-08-01 12:08:23 -0300

    .tdrop tip

--- .tdrop.sh
+++ .tdrop.sh
@@ -4,10 +4,11 @@
 # kitty is the terminal emulator
 # -ma is to use with multiple monitors
 # -h 100% is to use 100% of the screen height
-# -s is to create a tmux session (optional)
+# -s is to create a tmux session (optional) droped this now that I am using herdr
 #
-# Create a custom shortcut to run this command with a keybinding
-tdrop -ma -h 100% -s dropdown kitty
+# Create a custom shortcut to run this command with a keybinding (ex.: F12)
+tdrop -ma -h 100% kitty herdr
 
 # If needed add the --override argument in the kitty.desktop file
 # kitty -o hide_window_decorations=no
+
